e3cc02135170b4104e14f327fbbecdbd4efefd26
a.druzhkin <amdruzhkin@gmail.com>
2024-06-22 15:32:30 +0300

    SSL

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     uvicorn.run(app='main:app',
                 host='0.0.0.0',
                 port=443,
-                ssl_certfile='certificates/certificate_ca.crt',
+                ssl_certfile='certificates/certificate.crt',
                 ssl_keyfile='certificates/certificate.key')
 
 
