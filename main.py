f3bff51470676d146d7630867dcd60f72f7b991b
a.druzhkin <amdruzhkin@gmail.com>
2024-06-22 15:31:26 +0300

    SSL

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import pages
 
 app = FastAPI()
-app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     uvicorn.run(app='main:app',
                 host='0.0.0.0',
                 port=443,
-                ssl_certfile='certificate.crt',
-                ssl_keyfile='certificate.key')
+                ssl_certfile='certificates/certificate_ca.crt',
+                ssl_keyfile='certificates/certificate.key')
 
 
