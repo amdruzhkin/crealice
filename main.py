66c6117e38c7cefc7701592c04e938f034216b2a
a.druzhkin <amdruzhkin@gmail.com>
2024-06-29 15:58:00 +0300

    endpoints for robots

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ templates = Jinja2Templates(directory="templates")
 
 app.include_router(pages.router)
 
+# Function to check if the request is from a common search bot
+def is_search_bot(request: Request):
+    user_agent = request.headers.get("User-Agent", "").lower()
+    common_bots = ["googlebot", "yandexbot", "bingbot", "baiduspider"]
+    return any(bot in user_agent for bot in common_bots)
+
+@app.get("/robots.txt")
+async def robots(request: Request):
+    if is_search_bot(request):
+        robots_path = os.path.join("static", "robots.txt")
+        if os.path.exists(robots_path):
+            return FileResponse(robots_path, media_type="text/plain")
+        else:
+            raise HTTPException(status_code=404, detail="robots.txt not found")
+    else:
+        raise HTTPException(status_code=404, detail="Not Found")
+
+@app.get("/sitemap.xml")
+async def sitemap(request: Request):
+    if is_search_bot(request):
+        sitemap_path = os.path.join("static", "sitemap.xml")
+        if os.path.exists(sitemap_path):
+            return FileResponse(sitemap_path, media_type="application/xml")
+        else:
+            raise HTTPException(status_code=404, detail="sitemap.xml not found")
+    else:
+        raise HTTPException(status_code=404, detail="Not Found")
+
 if __name__ == '__main__':
     uvicorn.run(app='main:app',
                 host='0.0.0.0',
