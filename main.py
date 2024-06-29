93973f93b232155bf1c9bfb8766e97a5c7cb063d
a.druzhkin <amdruzhkin@gmail.com>
2024-06-29 15:41:19 +0300

    Remove certs and add robots.txt and sitemap.xml

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import pages
 
 app = FastAPI()
-# app.add_middleware(HTTPSRedirectMiddleware)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(pages.router)
@@ -15,8 +13,9 @@ app.include_router(pages.router)
 if __name__ == '__main__':
     uvicorn.run(app='main:app',
                 host='0.0.0.0',
-                port=443,
-                ssl_certfile='certificates/certificate.crt',
-                ssl_keyfile='certificates/certificate.key')
+                port=80)
+                # ssl_certfile='certificates/certificate.crt',
+                # ssl_keyfile='certificates/certificate.key',
+                # ssl_ca_certs='certificates/certificate_ca.crt')
 
 
